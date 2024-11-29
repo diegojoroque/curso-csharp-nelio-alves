69c9e7757dff22aaccca4e0a22e95486e1f6c78f
Diego Roque <diegojoroque@gmail.com>
2024-11-29 03:42:34 -0300

    new exercise

---
+++ Secao4Aula45ExercicioFixacaoClassAtribMet03/Secao4Aula45ExercicioFixacaoClassAtribMet03/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Secao4Aula45ExercicioFixacaoClassAtribMet03
+{
+    class Aluno
+    {
+        public string nome;
+        public double nota1, nota2, nota3;
+
+        public double NotaFinal()
+        {
+            return nota1 + nota2 + nota3;
+        }
+
+        public double PontosReprovacao()
+        {
+            return 60 - NotaFinal();
+        }
+
+        public string Aprovacao()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                return "REPROVADO" + "\n" + "FALTARAM " + PontosReprovacao().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS";
+            }
+        }
+        public override string ToString()
+        {
+            return "NOTA FINAL = " + NotaFinal().ToString("F2", CultureInfo.InvariantCulture) + "\n" + Aprovacao();
+        }
+    }
+}
--- Secao4Aula45ExercicioFixacaoClassAtribMet03/Secao4Aula45ExercicioFixacaoClassAtribMet03/Program.cs
+++ Secao4Aula45ExercicioFixacaoClassAtribMet03/Secao4Aula45ExercicioFixacaoClassAtribMet03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Secao4Aula45ExercicioFixacaoClassAtribMet03
 {
@@ -10,8 +11,16 @@ namespace Secao4Aula45ExercicioFixacaoClassAtribMet03
                 Exercício 03:
                 Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano  (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver este problema. 
             */
+            Aluno student = new Aluno();
 
+            Console.Write("Nome do aluno: ");
+            student.nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            student.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            Console.WriteLine(student);
         }
     }
 }
